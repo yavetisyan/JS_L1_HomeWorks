f39cbe2ec6248d4688ddb6f8eb559ceefd085080
yavetisyan <mr.avetisyan@yahoo.com>
2021-08-29 18:11:26 +0400

    remove file

--- Task_3.js
+++ Task_3.js
@@ -1,51 +1,13 @@
-//1. Учитывая массив.Напишите рекурсивную функцию, которая удаляет
-// Первый элемент и возвращает данный массив.(без использования
-// arr.unshift (), назначить второй элемент для первого третьего элемента
-//	второй...)
+// 1. Given an array. Write a recursive function that removes the first element and returns the given array. (without using arr.unshift(),assign second element to first, third element to second...)
 
-let x = [6, 78, "n", 0, 1];
-
-function removeArr(arr) {
-  //debugger;
-  let res = [];
-  let i = 0;
-  let tmp = [];
-  if (arr.length < 2) {
-    return;
-  } else {
-    arr[i] = arr[i + 1];
+function addArr() {
+  let length = +prompt("Enter Array Length");
+  arr = [];
+  for (let i = 0; i < length; i++) {
+    arr.push(+prompt("Enter Arrays"));
   }
-  res.push(arr[i]);
-
-  removeArr(arr.slice(1));
-  return res;
+  return arr;
 }
-console.log(removeArr(x));
-
-//let res = [];
-
-//for (let i = 0; i < arr.length; i++) {
-//	if (arr[i] === undefined) {
-//		break;
-//	} else {
-//		arr[i] = arr[i + 1];
-//		res.push(arr[i]);
-//	}
-//}
-//arr = res.pop();
-//return res;
-~(
-  // 1. Given an array. Write a recursive function that removes the first element and returns the given array. (without using arr.unshift(),assign second element to first, third element to second...)
-
-  function addArr() {
-    let length = +prompt("Enter Array Length");
-    arr = [];
-    for (let i = 0; i < length; i++) {
-      arr.push(+prompt("Enter Arrays"));
-    }
-    return arr;
-  }
-);
 
 //-----------------
 
@@ -83,10 +45,9 @@ function flateArr(arr) {
 
 console.log(flateArr(arr));
 
-
 // 3. Given a number. Write a function that calculates its sum of the digits and if that sum has more than 1 digit find the sum of digits of that number. Repeat that process if needed and return the result.
 
-et num = 99999;
+let num = 99999;
 
 function ff(n) {
   let res = n.toString();
@@ -107,14 +68,10 @@ function fn(num) {
   } else {
     sum = fn(ff(num));
   }
-
   return sum;
 }
 
-
 console.log(fn(num));
-
-
 
 // 4. Given an object. Invert it (keys become values and values become keys). If there is more than key for that given value create an array.
 
@@ -153,7 +110,6 @@ function invertObj(obj) {
 
 console.log(invertObj(obj));
 
-
 // 5.  Create a function that builds a tree like object given an array with object which contains parent and id properties.
 
 let treeNodes = [
@@ -165,23 +121,6 @@ let treeNodes = [
   { parent: 2, id: 5 },
   { parent: 4, id: 6 },
 ];
-
-
-function tree(arr, num = null) {
-  //debugger
-  let obj = {};
-
-  arr.forEach((el) => {
-    if (num === el.parent ) {
-      let tmp = tree(arr, el.id);
-      obj[el.id] = tmp;
-    }
-  });
-  return obj;
-}
-
-console.log(tree(treeNodes));
-
 
 function tree(arr, num = null) {
   let obj = {};
